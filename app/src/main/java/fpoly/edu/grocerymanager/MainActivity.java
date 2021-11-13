46a8dc8534a66f3caabbbe4178c859e482d5487b
Luong_Ngoc_Hai <luongngochaiqs@gmail.com>
2021-11-13 20:49:51 +0700

    thêm tất cả frament

--- app/src/main/java/fpoly/edu/grocerymanager/MainActivity.java
+++ app/src/main/java/fpoly/edu/grocerymanager/MainActivity.java
@@ -1,18 +1,160 @@
 package fpoly.edu.grocerymanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
-import fpoly.edu.grocerymanager.dao.DemoDB;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.navigation.NavigationView;
+
+import fpoly.edu.grocerymanager.fragment.AddUserFragment;
+import fpoly.edu.grocerymanager.fragment.DoanhThuFragment;
+import fpoly.edu.grocerymanager.fragment.DoiMatKhauFragment;
+import fpoly.edu.grocerymanager.fragment.HangFragment;
+import fpoly.edu.grocerymanager.fragment.LoaiHangFragment;
+import fpoly.edu.grocerymanager.fragment.QuanLyHoaDonFragment;
+import fpoly.edu.grocerymanager.fragment.TimKiemLoaiHangFragment;
+import fpoly.edu.grocerymanager.fragment.TimKiemMaHangFragment;
+import fpoly.edu.grocerymanager.fragment.TopFragment;
 
 public class MainActivity extends AppCompatActivity {
+        DrawerLayout drawer;
+        Toolbar toolbar;
+        View mHeaderView;
+        TextView edUser;
+        //ThuThuDAO thuThuDAO;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Tạo database
-        DemoDB demoDB = new DemoDB(getApplicationContext());
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            drawer = findViewById(R.id.drawer_layout);
+
+            toolbar = findViewById(R.id.toolbar1);
+
+            //set toolbar thay thế cho actionbar
+            setSupportActionBar(toolbar);
+            ActionBar ab = getSupportActionBar();
+
+            ab.setHomeAsUpIndicator(R.drawable.menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+
+            FragmentManager manager = getSupportFragmentManager();
+            QuanLyHoaDonFragment quanLyHoaDonFragment = new QuanLyHoaDonFragment();
+            manager.beginTransaction()
+                    .replace(R.id.flContent, quanLyHoaDonFragment).commit();
+
+            NavigationView nv = findViewById(R.id.nvView);
+            //show user in header
+            mHeaderView = nv.getHeaderView(0);
+            edUser = mHeaderView.findViewById(R.id.tvtUser);
+
+//            Intent i = getIntent();
+//            String user = i.getStringExtra("user");
+//            thuThuDAO = new ThuThuDAO(this);
+//            ThuThu thuThu = thuThuDAO.getID(user);
+//            String username = thuThu.getHoTen();
+            //           edUser.setText("Xin chào "+username+" !");
+
+//            if (user.equalsIgnoreCase("admin")){
+//                nv.getMenu().findItem(R.id.sub_Adduser).setVisible(true);
+//            }
+
+            nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    FragmentManager manager = getSupportFragmentManager();
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_HoaDon:
+                            setTitle("Quản lý hoá đơn");
+                            QuanLyHoaDonFragment quanLyHoaDonFragment = new QuanLyHoaDonFragment();
+                            manager.beginTransaction()
+                                    .replace(R.id.flContent, quanLyHoaDonFragment).commit();
+                            break;
+                        case R.id.nav_LoaiHang:
+                            setTitle("Quản lý loại hàng");
+                            LoaiHangFragment loaiHangFragment = new LoaiHangFragment();
+                            manager.beginTransaction()
+                                    .replace(R.id.flContent, loaiHangFragment).commit();
+
+                            break;
+                        case R.id.nav_Hang:
+                            setTitle("Quản lý hàng hoá");
+                            HangFragment hangFragment = new HangFragment();
+                            manager.beginTransaction()
+                                    .replace(R.id.flContent, hangFragment).commit();
+
+                            break;
+                        case R.id.nav_TimKiemLH:
+                            setTitle("Tìm kiếm theo loại hàng");
+                            TimKiemLoaiHangFragment timKiemLoaiHangFragment = new TimKiemLoaiHangFragment();
+                            manager.beginTransaction().replace(R.id.flContent, timKiemLoaiHangFragment)
+                                    .commit();
+
+                            break;
+                        case R.id.nav_TimKiemMH:
+                            setTitle("Tìm kiếm theo mã hàng");
+                            TimKiemMaHangFragment timKiemMaHangFragment = new TimKiemMaHangFragment();
+                            manager.beginTransaction().replace(R.id.flContent, timKiemMaHangFragment)
+                                    .commit();
+
+                            break;
+                        case R.id.sub_Top:
+                            setTitle("Top 10 hàng hoá được mua nhiều nhất");
+                            TopFragment topFragment = new TopFragment();
+                            manager.beginTransaction()
+                                    .replace(R.id.flContent, topFragment).commit();
+                            break;
+                        case R.id.sub_DoanhThu:
+                            setTitle("Thống kê Doanh thu");
+                            DoanhThuFragment doanhThuFragment = new DoanhThuFragment();
+                            manager.beginTransaction()
+                                    .replace(R.id.flContent, doanhThuFragment).commit();
+                            break;
+                        case R.id.sub_Adduser:
+                            setTitle("Thêm người dùng");
+                            AddUserFragment addUserFragment = new AddUserFragment();
+                            manager.beginTransaction().replace(R.id.flContent, addUserFragment)
+                                    .commit();
+
+                            break;
+                        case R.id.sub_Pass:
+                            setTitle("Thay đổi mật khẩu");
+                            DoiMatKhauFragment doiMatKhauFragment = new DoiMatKhauFragment();
+                            manager.beginTransaction().replace(R.id.flContent, doiMatKhauFragment)
+                                    .commit();
+                            break;
+                        case R.id.sub_Logout:
+                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                            finish();
+
+                            break;
+
+                    }
+                    drawer.closeDrawers();
+
+                    return false;
+                }
+            });
+
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home)
+                drawer.openDrawer(GravityCompat.START);
+            return super.onOptionsItemSelected(item);
+        }
     }
-}
