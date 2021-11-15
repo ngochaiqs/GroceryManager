40dafb76ffe8d01f1491cb051058e0bdbec29426
ngochaiqs <luongngochaiqs@gmail.com>
2021-11-15 16:45:31 +0700

    xong splash và login

--- app/src/main/java/fpoly/edu/grocerymanager/SplashActivity.java
+++ app/src/main/java/fpoly/edu/grocerymanager/SplashActivity.java
@@ -1,10 +1,6 @@
 package fpoly.edu.grocerymanager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,15 +10,5 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },1000);
     }
 }
